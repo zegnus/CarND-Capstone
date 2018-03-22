af3e1b57705c0ff7b15a43839cde21a7cdae647c
asimay <asimay_y@126.com>
2018-03-22 13:43:49 +0800

    add comments and optimize some code

--- ros/src/twist_controller/src/dbw_node_core.cpp
+++ ros/src/twist_controller/src/dbw_node_core.cpp
@@ -30,28 +30,31 @@ void DBWNode::initForROS()
     private_nh_.param<double>("max_lat_accel", max_lat_accel_, 3.0);
     private_nh_.param<double>("max_steer_angle", max_steer_angle_, 8.0);
 
-    //TODO: min_speed_
+    //set basic parameters for yaw controller.
     yaw_controller_.setParameters(wheel_base_, steer_ratio_, max_lat_accel_, max_steer_angle_);
 
+    //steer, throttle, brake publisher
     steer_pub_ = nh_.advertise<dbw_mkz_msgs::SteeringCmd>("/vehicle/steering_cmd", 2);
     throttle_pub_ = nh_.advertise<dbw_mkz_msgs::ThrottleCmd>("/vehicle/throttle_cmd", 2);
     brake_pub_ = nh_.advertise<dbw_mkz_msgs::BrakeCmd>("/vehicle/brake_cmd", 2);
 
+    //twist_cmd, dbw_enabled, current_velocity,steering_report subscriber
     sub_vel_ = nh_.subscribe("/twist_cmd", 2, &DBWNode::cbFromTwistCmd, this);
-    sub_enable_ = nh_.subscribe("/vehicle/dbw_enabled", 2, &DBWNode::cbFromRecvEnable, this);
+    sub_enable_ = nh_.subscribe("/vehicle/dbw_enabled", 1, &DBWNode::cbFromRecvEnable, this);
     sub_cur_vel_ = nh_.subscribe("/current_velocity", 2, &DBWNode::cbFromCurrentVelocity, this);
     sub_steer_report_ = nh_.subscribe("/vehicle/steering_report", 2, &DBWNode::cbFromSteeringReport, this);
 
-    //give a general value for MKZ.
+    //give a general value for MKZ, MKZ gas container is 63.
     lpf_fuel_.setParams(63.0, 0.1); 
     //assume 50% Fuel level
     lpf_fuel_.filt(50);
     //ROS_INFO("DBW_node, lpf_fuel_.get(): %f", lpf_fuel_.get());
     //ROS_INFO("DBW_node, lpf_fuel_.getReady(): %d", lpf_fuel_.getReady());
 
-    //TODO accel_limit_
+    //lowpass accel, 
     lpf_accel_.setParams(0.5, 0.02);
 
+    //set speed pid range for tune
     speed_pid_.setRange(decel_limit_, accel_limit_);
     accel_pid_.setRange(0, accel_limit_);
 }
@@ -66,9 +69,11 @@ void DBWNode::run()
 
         if(sys_enable_ == true)
         {
+            //get controller's value, including throttle, steering, brake
             getPredictedControlValues();
         }
-        else
+        else    
+        /*if enable is false, means manually drive, should reset PID parameter to avoid accumulate error.*/
         {
             speed_pid_.resetError();
             accel_pid_.resetError();
@@ -79,11 +84,12 @@ void DBWNode::run()
     }
 }
 
-
+/* Publish control command */
 void DBWNode::publishControlCmd(Controller v_controller)
 {
     dbw_mkz_msgs::ThrottleCmd tcmd = dbw_mkz_msgs::ThrottleCmd();
     tcmd.enable = true;
+    // throttle is percentage
     tcmd.pedal_cmd_type = dbw_mkz_msgs::ThrottleCmd::CMD_PERCENT;
     tcmd.pedal_cmd = v_controller.throttle;
     this->throttle_pub_.publish(tcmd);
@@ -95,35 +101,46 @@ void DBWNode::publishControlCmd(Controller v_controller)
 
     dbw_mkz_msgs::BrakeCmd bcmd = dbw_mkz_msgs::BrakeCmd();
     bcmd.enable = true;
+    //brake is torque (N*m)
     bcmd.pedal_cmd_type = dbw_mkz_msgs::BrakeCmd::CMD_TORQUE;
     bcmd.pedal_cmd = v_controller.brake;
     this->brake_pub_.publish(bcmd);
-
 }
 
+/* Listen dbw_enable message */
 void DBWNode::cbFromRecvEnable(const std_msgs::Bool::ConstPtr& msg)
 {
     sys_enable_ = msg->data;
 }
 
+/* Listen twist_cmd message, call back function */
 void DBWNode::cbFromTwistCmd(const geometry_msgs::TwistStamped::ConstPtr& msg)
 {
     twist_cmd_.header = msg->header;
     twist_cmd_.twist = msg->twist;
 }
 
+/* Listen cur_velocity message, which is from simulator, call back function */
 void DBWNode::cbFromCurrentVelocity(const geometry_msgs::TwistStamped::ConstPtr& msg)
 {
     cur_velocity_.header = msg->header;
     cur_velocity_.twist = msg->twist;
 }
 
+/* Listen SteeringReport message, which is actual value, call back function */
 void DBWNode::cbFromSteeringReport(const dbw_mkz_msgs::SteeringReport::ConstPtr& msg)
 {
     double raw_accel = LOOP_RATE * (msg->speed - cur_velocity_.twist.linear.x);
     lpf_accel_.filt(raw_accel);
+
+    if(fabs(msg->speed - cur_velocity_.twist.linear.x) < (double)1e-2)
+    {
+        cur_velocity_.twist.linear.x = msg->speed;
+    }
+
 }
 
+/* Listen Dynamic Reconfig message, which is actual value, call back function */
 void DBWNode::cbFromDynamicReconfig(twist_controller::ControllerConfig& config, uint32_t level)
 {
     cfg_ = config;
@@ -133,13 +150,12 @@ void DBWNode::cbFromDynamicReconfig(twist_controller::ControllerConfig& config, 
     accel_pid_.setGains(cfg_.accel_kp, cfg_.accel_ki, cfg_.accel_kd);
 }
 
+/* get controller's value, including throttle, steering, brake */
 void DBWNode::getPredictedControlValues()
 {
-
     // vehicle mass calculation
     double vehicle_mass = vehicle_mass_ + lpf_fuel_.get() / 100.0 * fuel_capacity_ * GAS_DENSITY;
 
-    //for simulator, for real car, should fetch from report
     double vel_cte = twist_cmd_.twist.linear.x - cur_velocity_.twist.linear.x;
     
     if(fabs(twist_cmd_.twist.linear.x) < mphToMps(1.0))
@@ -147,18 +163,18 @@ void DBWNode::getPredictedControlValues()
         speed_pid_.resetError();
     }
 
-    //speed pid
-    double accel_cmd = speed_pid_.step(vel_cte, control_gap_);
+    //speed pid, get adjusted speed_cmd
+    double speed_cmd = speed_pid_.step(vel_cte, control_gap_);
 
     if(twist_cmd_.twist.linear.x <= (double)1e-2)
     {
-        accel_cmd = std::min(accel_cmd, -530 / vehicle_mass / wheel_radius_);
+        speed_cmd = std::min(speed_cmd, -530 / vehicle_mass / wheel_radius_);
     }
 
     //set throttle signal
-    if(accel_cmd >= 0.0)
+    if(speed_cmd >= 0.0)
     {
-        vehicle_controller_.throttle = accel_pid_.step(accel_cmd - lpf_accel_.get(), control_gap_);
+        vehicle_controller_.throttle = accel_pid_.step(speed_cmd - lpf_accel_.get(), control_gap_);
     }
     else
     {
@@ -167,9 +183,10 @@ void DBWNode::getPredictedControlValues()
     }
 
     //set brake signal
-    if (accel_cmd < -brake_deadband_) 
+    if (speed_cmd < -brake_deadband_) 
     {
-        vehicle_controller_.brake = -accel_cmd * vehicle_mass * wheel_base_;
+        //get vehicle brake torque, T = m * a * r
+        vehicle_controller_.brake = -speed_cmd * vehicle_mass * wheel_base_;
         //ROS_INFO("vehicle_controller_.brake: %f", vehicle_controller_.brake);
     } 
     else 
@@ -177,14 +194,14 @@ void DBWNode::getPredictedControlValues()
         vehicle_controller_.brake = 0;
     }
 
-    //set steering signal
+    //set steering signal, only use P controller
     double steering_wheel = yaw_controller_.get_steering(twist_cmd_.twist.linear.x, twist_cmd_.twist.angular.z, 
-                                                    cur_velocity_.twist.linear.x);
+                                                        cur_velocity_.twist.linear.x);
     vehicle_controller_.steer = steering_wheel + cfg_.steer_kp * (twist_cmd_.twist.angular.z - cur_velocity_.twist.angular.z);
 
     //TODO:: for simulator test to see work or not, remember to remove
-    //vehicle_controller_.throttle = 0.06;
-    //vehicle_controller_.steer = 0.01;    
+    //vehicle_controller_.throttle = 0.3;
+    //vehicle_controller_.steer = 0.0;    
     //vehicle_controller_.brake = 0;
 
     publishControlCmd(vehicle_controller_);
